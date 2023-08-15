83a531a01558d74bdfbdf114c2454b31e8356915
harshida <harshidack50@gmail.com>
2023-08-15 11:58:17 +0530

    routing added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,17 @@ import { Foods } from '../shared/models/food';
 export class HomeComponent {
 
   foods: Foods[] = []
-  constructor(private foodService: FoodService) { }
+  constructor(private foodService: FoodService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.foods = this.foodService.getAll()
+    this.route.params.subscribe(params => {
+      if (params['searchItem']) {
+        this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+      } else {
+        this.foods = this.foodService.getAll();
+      }
+    })
   }
 
 }
